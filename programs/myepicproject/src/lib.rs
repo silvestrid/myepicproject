67a5fc4d1f5a046c0db7d3394b735c8e5deb3e54
Davide Silvestri <silvestri.eng@gmail.com>
2022-01-03 17:07:56 +0100

    anchor deploy to Solana devnet

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AqYeE39uDeQz8yCSMswbJrqzbd6KXVfiZPWsyoKWPVbj");
 
 #[program]
 pub mod myepicproject {
