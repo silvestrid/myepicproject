bd16bfad5399966766a7f897d2c182fc37d3d906
Davide Silvestri <silvestri.eng@gmail.com>
2022-01-03 16:35:26 +0100

    first Solana program

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -5,10 +5,10 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod myepicproject {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
