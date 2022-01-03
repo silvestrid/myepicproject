2f6756a7e5c34fa60b7bcad44245f06a3912dae8
Davide Silvestri <silvestri.eng@gmail.com>
2022-01-03 16:43:43 +0100

    store gif struct on our program

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -11,10 +11,19 @@ pub mod myepicproject {
         Ok(())
     }
 
-    // Another function woo!
-    pub fn add_gif(_ctx: Context<AddGif>) -> ProgramResult {
-        // Get a reference to the account and increment total_gifs.
+    // The function now accepts a gif_link param from the user. We also reference the user from the Context
+    pub fn add_gif(_ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
         let base_account = &mut _ctx.accounts.base_account;
+        let user = &mut _ctx.accounts.user;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
         base_account.total_gifs += 1;
         Ok(())
     }
@@ -29,15 +38,25 @@ pub struct StartStuffOff<'info> {
     pub system_program: Program<'info, System>,
 }
 
-// Specify what data you want in the AddGif Context.
-// Getting a handle on the flow of things :)?
+// Add the signer who calls the AddGif method to the struct so that we can save it
 #[derive(Accounts)]
 pub struct AddGif<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
 }
 
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
 }
